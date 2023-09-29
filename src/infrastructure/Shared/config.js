509398c0879a911f22360439f06f98df5c84068e
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2023-09-29 19:33:00 +0100

    Rename env variables

--- src/infrastructure/Shared/config.js
+++ src/infrastructure/Shared/config.js
@@ -5,10 +5,10 @@ export const config = {
   mailgun: {
     domain: "sandbox438c8dd938f0410aa1dd0393b97f4f46.mailgun.org",
     authUser: "api",
-    apiKey: process.env.API_MAILGUN,
+    apiKey: process.env.MAILGUN_API_KEY,
   },
   testInbox: {
-    apiKey: process.env.API_TESTMAIL,
+    apiKey: process.env.TESTMAIL_API_KEY,
     namespace: "kcat1",
   },
   mongo: {
