b34cd3bcc87632054c96b5a1d4de4c88384be490
Dani Tallón <kalimero99@protonmail.ch>
2023-10-19 17:34:15 +0200

    add LoginUser tests

--- src/application/LoginUser.js
+++ src/application/LoginUser.js
@@ -1,3 +1,6 @@
+import { UserNotFoundError } from "../domain/errors/UserNotFoundError.js"
+import {WrongPasswordError} from "../domain/errors/WrongPasswordError.js"
+
 export class LoginUser {
   constructor(userRepository, jsonWebTokenManager) {
     this.userRepository = userRepository
@@ -5,7 +8,15 @@ export class LoginUser {
   }
 
   async execute(email, password) {
-    const user = await this.userRepository.findByEmail(email)
+    const user = await this.userRepository.findByEmail(email, password)
+
+    if (!user) {
+      throw new UserNotFoundError
+    }
+
+    if (!user.compareWith(password)) {
+      throw new WrongPasswordError
+    }
 
     return this.jsonWebTokenManager.generate(user.getId())
   }
--- src/application/LoginUser.test.js
+++ src/application/LoginUser.test.js
@@ -5,6 +5,7 @@ import { User } from "../domain/models/User.js"
 import { JsonWebTokenManagerMock } from "../infrastructure/JsonWebTokenManager/JsonWebTokenManagerMock.js"
 import { LoginUser } from "./LoginUser.js"
 import { UserNotFoundError } from "../domain/errors/UserNotFoundError.js"
+import { WrongPasswordError } from "../domain/errors/WrongPasswordError.js"
 
 describe("LoginUser", () => {
   let userRepository
@@ -15,6 +16,14 @@ describe("LoginUser", () => {
     jsonWebTokenManager = new JsonWebTokenManagerMock()
   })
 
+  it("must find the user by email"), async () => {
+    const email = "manolo@gmail.com"
+    const loginUser = new LoginUser(userRepository, jsonWebTokenManager)
+    await loginUser.execute(email, "password")
+
+    expect(userRepository.findByEmail).toHaveBeenCalledWith(email)
+  }
+
   it("generates a JWT for the given user", async () => {
     const name = "manolo"
     const email = "manolo@gmail.com"
@@ -24,6 +33,7 @@ describe("LoginUser", () => {
     const tokenMock = "thisIsAJWTSignedToken"
     vi.spyOn(userRepository, "findByEmail").mockReturnValue(user)
     vi.spyOn(jsonWebTokenManager, "generate").mockReturnValue(tokenMock)
+
     const loginUser = new LoginUser(userRepository, jsonWebTokenManager)
 
     const token = await loginUser.execute(email, password)
@@ -32,13 +42,30 @@ describe("LoginUser", () => {
   })
 
   it("throws an error if user doesn't exists", async () => {
-    const name = "manolo"
     const email = "manolo@gmail.com"
+
+
     vi.spyOn(userRepository, "findByEmail").mockReturnValue(null)
     const loginUser = new LoginUser(userRepository, jsonWebTokenManager)
 
-    const result = loginUser.execute(email, password)
+    const result = loginUser.execute(email)
 
     await expect(result).rejects.toBeInstanceOf(UserNotFoundError)
+  })
+
+  it("throws an error is password is wrong", async () => {
+    const name = "manolo"
+    const email = "manolo@gmail.com"
+    const age = 45
+    const password = "123456"
+    const user = User.create(IdGeneratorMock.MOCK_ID, name, email, password, age)
+
+
+    vi.spyOn(userRepository, "findByEmail").mockReturnValue(user)
+    const loginUser = new LoginUser(userRepository, jsonWebTokenManager)
+    
+    const result = loginUser.execute(email, "invalidPassword")
+    
+    await expect(result).rejects.toBeInstanceOf(WrongPasswordError)
   })
 })
